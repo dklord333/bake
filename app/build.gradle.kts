1becfe9b2c091f3167d102ea170b40d0b73026f0
knight <none@none.com>
2025-01-09 14:39:55 +0500

    added quantity selector

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.internal.file.pattern.PatternMatcherFactory.compile
+import java.util.regex.Pattern.compile
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -29,10 +32,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+}
+buildscript {
+    repositories {
+        jcenter()
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.travijuu:numberpicker:1.0.7")
+    implementation("com.shawnlin:number-picker:2.4.2'")
+
 }
